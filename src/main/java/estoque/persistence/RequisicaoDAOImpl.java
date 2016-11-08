50b19138437b7898b9ff94b940ff41fa978e8178
MateusAndrade <mateus.andrade47@outlook.com>
2016-11-08 12:38:02 -0200

    Verificar tratamento de loign, faltando finalizar filtros e updates

--- src/main/java/estoque/persistence/RequisicaoDAOImpl.java
+++ src/main/java/estoque/persistence/RequisicaoDAOImpl.java
@@ -74,4 +74,28 @@ public class RequisicaoDAOImpl implements IRequisicaoDAO {
 		ps.close();
 	}
 
+	@Override
+	public List<Requisicao> listaSaidaCaixa() throws SQLException {
+		String sql = "CALL consultaSaidaCaixa();";
+		List<Requisicao> listaRequisicao = new ArrayList<Requisicao>();
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()){
+			Requisicao req = new Requisicao();
+			Funcionario func = new Funcionario();
+			Produto prod = new Produto();
+			prod.setCodigo((rs.getInt("codigo_produto")));
+			prod.setNome((rs.getString("nome_produto")));
+			prod.setPreco((rs.getDouble("valor_caixa")));
+			req.setFunc(func);
+			req.setProd(prod);
+			req.setQtd_requisicao((rs.getInt("qtd_requisicao")));
+			listaRequisicao.add(req);
+		}
+		rs.close();
+		ps.close();		
+		System.out.println(listaRequisicao);
+		return listaRequisicao;
+	}
+
 }
